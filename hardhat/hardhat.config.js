fb3cf503f0dde8b1f92682c00eab2bcfe641ef80
Tebbo <leetebbs@ymail.com>
2024-05-16 18:41:14 +0100

    add deploy script and config for amoy

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    polygonAmoy: {
+      url: process.env.POLYGON_AMOY_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+      gasPrice: "auto",
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: process.env.OKLINK_AMOY_API,
+    },
+    customChains: [
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://www.oklink.com/api/explorer/v1/polygonamoy/contract/verify/async",
+          browserURL: "https://www.oklink.com/amoy",
+        },
+      },
+    ],
+  },
 };
